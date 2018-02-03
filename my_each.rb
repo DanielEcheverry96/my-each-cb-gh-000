2a4c92bfad33240ed39c7d13aed6d7261dd79670
Daniel Echeverry Giraldo <dfecheverryg@unal.edu.co>
2018-02-03 00:19:33 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,24 @@
 def my_each (array)# put argument(s) here
   # code here
-  index = 0
-  while index < array.lenght
-    yield (array[index])
-    index += 1
+  if condition
+    index = 0
+    while index < array.lenght
+      yield (array[index])
+      index += 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
   end
-  array
 end
+
+if block_given?
+    i = 0
+    while i < array.length
+      yield (array[i])
+      i += 1
+    end
+    array
+  else
+    puts "Hey! No block was given!"
+  end
